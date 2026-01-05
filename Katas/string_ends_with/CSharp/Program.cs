4a2fd3fa8ec0e36353705705749abf090404b001
Gthierry <thierrygillot@live.com>
2026-01-05 15:09:40 +0100

    bug c# string with ends //TODO

--- Katas/string_ends_with/CSharp/Program.cs
+++ Katas/string_ends_with/CSharp/Program.cs
@@ -1,4 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using CSharp;
 
+
 Console.WriteLine(StringEndsWith.String_Ends_With("abc",""));
--- Katas/string_ends_with/CSharp/obj/Debug/net9.0/CSharp.AssemblyInfo.cs
+++ Katas/string_ends_with/CSharp/obj/Debug/net9.0/CSharp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CSharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da976c3b3ddd47fb7d20a3de8dbf248abd2ed9fe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d22e804ba2cbc299b7a315e8e90a257f513a00f")]
 [assembly: System.Reflection.AssemblyProductAttribute("CSharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CSharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
